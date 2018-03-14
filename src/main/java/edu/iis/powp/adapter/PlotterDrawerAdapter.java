57e6731952583af3c262ea36d6f3122eadefb056
Marcin Kapelan <marcinkape@gmail.com>
2018-03-14 23:19:11 +0100

    Rename MyAdapter class to PlotterDrawerAdapter

--- src/main/java/edu/iis/powp/adapter/PlotterDrawerAdapter.java
+++ src/main/java/edu/iis/powp/adapter/PlotterDrawerAdapter.java
@@ -10,11 +10,11 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
+public class PlotterDrawerAdapter extends DrawPanelController implements IPlotter
 { 
 	private int startX = 0, startY = 0;
 	
-    public MyAdapter() {
+    public PlotterDrawerAdapter() {
 		super();
 	}
     
--- src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
+++ src/test/java/edu/iis/powp/gui/TestPlotSoftPatterns.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 import edu.iis.client.plottermagic.ClientPlotter;
 import edu.iis.client.plottermagic.IPlotter;
-import edu.iis.powp.adapter.MyAdapter;
+import edu.iis.powp.adapter.PlotterDrawerAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.Context;
 import edu.iis.powp.app.DriverManager;
@@ -46,7 +46,7 @@ public class TestPlotSoftPatterns
 		context.addDriver("Client Plotter", clientPlotter);
 		Application.getComponent(DriverManager.class).setCurrentPlotter(clientPlotter);
 		
-		IPlotter plotter = new MyAdapter();
+		IPlotter plotter = new PlotterDrawerAdapter();
 		context.addDriver("Buggy Simulator", plotter);
 
 		context.updateDriverInfo();
