666dab15a34c67a636c7247eedaf84e6d90af977
Marcin Kapelan <194978@edu.p.lodz.pl>
2018-03-22 14:51:07 +0100

    Add CommandSetPosition class

---
+++ src/main/java/edu/iis/powp/command/CommandSetPosition.java
@@ -0,0 +1,18 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandSetPosition implements PlotterCommand {
+	private int x;
+	private int y;
+	
+	public CommandSetPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public void execute(IPlotter plotter) {
+		plotter.setPosition(x, y);
+	}
+}
